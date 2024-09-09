7c59048026d1a6c41f24083b8e4905bbd4d9657e
Oscar <hieu.ho@digitalunicorn.fr>
2024-09-09 20:52:37 +0700

    First

---
+++ preload.js
@@ -0,0 +1,22 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+// Expose a limited API to the renderer process
+contextBridge.exposeInMainWorld('electronAPI', {
+  login: (account) => ipcRenderer.invoke('login', account),
+  test: (account) => ipcRenderer.invoke('test', account),
+  run: (account) => ipcRenderer.invoke('run', account),
+  updateStatus: (account) => ipcRenderer.invoke('update-status', account),
+  onActionResult: (callback) => ipcRenderer.on('action-result', (event, result) => callback(result)),
+  selectFolder: () => ipcRenderer.invoke('select-folder'),
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+})
