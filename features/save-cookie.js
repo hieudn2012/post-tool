4fd24bae2cdd6062c420929108cb3941f30b1288
Oscar <hieu.dn2012@gmail.com>
2024-09-15 12:24:14 +0700

    Update save cookie

--- features/save-cookie.js
+++ features/save-cookie.js
@@ -1,11 +1,12 @@
 import fs from 'node:fs';
-import { THREADS_LOGIN_URL } from "../src/constants/common.js";
+import { THEAD_URL, THREADS_LOGIN_URL } from "../src/constants/common.js";
 import { getRootPath, sendEvent } from "./common.js";
 
 // save cookies
 async function saveCookie({ account, event, pages, browsers, userAgents }) {
   const folderPath = getRootPath();
-  const page = pages[`${account.id}_${THREADS_LOGIN_URL}`];
+
+  const page = pages[`${account.id}_${THREADS_LOGIN_URL}`] || pages[`${account.id}_${THEAD_URL}`];
   if (!page) {
     sendEvent({ account, event, status: 'No page found' });
     throw new Error(`No page found with ID ${account.id}.`);
