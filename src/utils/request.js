18e5403ecccc4006cb90c315907d040e95f64950
Oscar <hieu.dn2012@gmail.com>
2024-09-23 23:01:40 +0700

    API URL

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-import { APP_API_URL } from '../constants/common.js';
+import { API_URL } from '../constants/common.js';
 import store from './store.js';
 
 const request = axios.create({
-  baseURL: APP_API_URL,
+  baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${store.get('token')}`,
