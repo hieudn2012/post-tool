060777c250fbebe7284f7ca55e6f035dfbe01665
Oscar <hieu.dn2012@gmail.com>
2024-09-24 09:37:56 +0700

    Add valid account for start

--- src/features/get-accounts.js
+++ src/features/get-accounts.js
@@ -1,6 +1,7 @@
+import _ from 'lodash';
 import request from "../utils/request.js";
 
 export const getAccounts = async () => {
   const { data } = await request.get('/accounts');
-  return data;
+  return _.filter(data, ({ category, access_token, account, threads_info }) => category && access_token && account === threads_info?.username);
 };
