047fe10946649cff88106c973911526d5046b5d4
Oscar <hieu.dn2012@gmail.com>
2024-10-16 10:03:27 +0700

    fix account not have proxy

--- src/features/get-accounts.js
+++ src/features/get-accounts.js
@@ -1,6 +1,7 @@
+import _ from "lodash";
 import request from "../utils/request.js";
 
 export const getAccounts = async (status) => {
   const { data } = await request.get(`/accounts/for-run/all?status=${status}`);
-  return data;
+  return _.filter(data, (account) => !!account.proxy);
 };
