fc91abed073999fdc154e1372ccff1e9212480b9
Oscar <hieu.dn2012@gmail.com>
2024-10-23 17:51:59 +0700

    Fix: change base url

--- src/constants/common.js
+++ src/constants/common.js
@@ -2,7 +2,7 @@ const THREADS_LOGIN_URL = 'https://www.threads.net/login/';
 const INSTAGRAM_URL = 'https://www.instagram.com/';
 const THEAD_URL = 'https://www.threads.net/';
 
-const API_URL = 'http:localhost:3000';
+const API_URL = 'https://socialfly-be.fly.dev';
 
 export {
   THREADS_LOGIN_URL,
